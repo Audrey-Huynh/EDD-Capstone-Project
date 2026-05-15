8081437225d7a7d1b42cf67ebefed94b886a0f4b
HackingGo306 <22camerjian@gmail.com>
2026-05-14 17:59:15 -0700

    mysql pool url

--- model/pool.js
+++ model/pool.js
@@ -5,24 +5,7 @@ let pool = null;
 // Seperate development and production databases
 if (process.env.NODE_ENV === "production") {
   console.log("Using production database configuration.");
-  pool = mysql.createPool({
-    host: process.env.MARIA_HOST,
-    user: process.env.MARIA_USER,
-    password: process.env.MARIA_PASSWORD,
-    database: process.env.MARIA_DATABASE,
-    port: process.env.MARIA_PORT || 3306,
-    ssl: {
-      ca: process.env.DATABASE_SSL_CA,
-    },
-
-    connectTimeout: 10000,
-    multipleStatements: true,
-    connectionLimit: 100,
-    waitForConnections: true,
-    debug: false,
-    charset: "utf8mb4",
-    idleTimeout: 60000,
-  });
+  pool = mysql.createPool(process.env.MYSQL_URL);
 } else {
   console.log("Using development database configuration.");
   pool = mysql.createPool({
